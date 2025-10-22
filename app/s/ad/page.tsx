c829f118d1a3e3c3cd8f507dd408566434ad9b12
kncseller <kncseller@gmail.com>
2025-10-22 13:59:24 +0700

    v1

---
+++ app/s/ad/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import Script from 'next/script';
+
+export default function Home() {
+  return (
+    <>
+    <link rel="stylesheet" href="/s/ad/font.css" /> 
+    <div className="">
+        hêllo
+    </div>
+ 
+
+  
+    </>
+  );
+}
